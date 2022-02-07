ea10d5986dc42071cd976bd254cf1f3604774008
Obinna Okwuolisa <o.okwuolisa@dickblick.com>
2022-02-07 13:31:11 +0100

    upload constants

--- uploader/src/constants.ts
+++ uploader/src/constants.ts
@@ -1,5 +1,7 @@
-export const S3_BUCKET = 'lagosmp4videogracccie';
-export const REGION = 'us-east-1';
+export const S3_BUCKET = process.env.REACT_APP_S3_BUCKET
+  ? process.env.REACT_APP_S3_BUCKET
+  : '';
+export const REGION = process.env.REACT_APP_REGION;
 export const SIZE_LIMIT = 50000000;
 export const ACCESS_KEY_ID = process.env.REACT_APP_ACCESS_KEY_ID;
 export const SECRET_ACCESS_KEY = process.env.REACT_APP_SECRET_ACCESS_KEY;
